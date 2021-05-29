ccb23ccc8f1ca55f0766d79d56340705d08d51ee
kzaman3055 <81049021+kzaman3055@users.noreply.github.com>
2021-05-29 15:09:43 +0600

    Delete Countword.py

--- Countword.py
+++
@@ -1,9 +0,0 @@
-file = open("/home/polash/test.txt", "r")
-number_of_words = 0
-for word in file:
-  print("Text in the file:",word)
-  word = word.strip("\n")
-  words = len(word.split())
-  number_of_words += words
-file.close()
-print("Total words in the file: ", number_of_words)
